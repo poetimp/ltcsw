3c24647db25b48749ba3c04f0d53582ff00fe569
paullem <paullem@e5b4602d-4317-41f7-8a30-07176c2d51a0>
2015-01-04 04:45:46 +0000

    FIxed problem with deleting rooms

--- registration/DelRoom.php
+++ registration/DelRoom.php
@@ -1,24 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en">
 <?php
 include 'include/RegFunctions.php';
+$RoomID=$_REQUEST['RoomID'];
+$RoomName = getRoomName($RoomID);
 
 if (isset($_POST['Confirm']))
 {
-   $RoomName=$_REQUEST['RoomName'];
-
-   mysql_query("delete from $RoomsTable where RoomName='$RoomName'")
+   mysql_query("delete from $RoomsTable where RoomID='$RoomID'")
        or die ("Unable to delete Room record: " . mysql_error());
 
    ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html lang="en">
       <head>
          <title>
             Room Deleted
          </title>
       </head>
       <body style="background-color: rgb(217, 217, 255);">
-         <h1 align=center>
-            Room <?php  print $_REQUEST['RoomName']; ?> Deleted!
+         <h1 align="center">
+            Room <?php  print $RoomName; ?> Deleted!
          </h1>
          <?php footer("Return to Room List","Rooms.php")?>
 
@@ -33,8 +33,6 @@ else if (isset($_POST['Cancel']))
 else
 {
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html lang="en">
 
        <head>
           <title>
@@ -43,20 +41,20 @@ else
        </head>
 
        <body style="background-color: rgb(217, 217, 255);">
-          <form method="post" action="DelRoom.php<?php  print "?RoomName=".urlencode($_REQUEST['RoomName']); ?>">
+          <form method="post" action="DelRoom.php<?php  print "?RoomID=$RoomID"; ?>">
              <center>
                 <h1>
                    Deleting Room
                 </h1>
                 <h2>
-                   "<?php  print $_REQUEST['RoomName']; ?>"
+                   "<?php  print $RoomName; ?>"
                 </h2>
              </center>
              <p align="center">
-             <input type="submit" value="Confirm Delete!" name="Confirm">
-             <font size="5"><br>
-             or</font><br>
-             <input type="submit" value="Cancel" name="Cancel">
+             <input type="submit" value="Confirm Delete!" name="Confirm"/>
+             <font size="5"><br/>
+             or</font><br/>
+             <input type="submit" value="Cancel" name="Cancel"/>
              </p>
           </form>
        </body>
