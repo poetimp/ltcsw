b820f50995a1e5a32ee1ce28a304b0b66f2cc5f7
paullem <paullem@e5b4602d-4317-41f7-8a30-07176c2d51a0>
2015-09-13 01:22:21 +0000

    Updated the awards counts to reflect team member counts instead of just teams.

--- registration/RptAwardCounts.php
+++ registration/RptAwardCounts.php
@@ -6,6 +6,10 @@ if ($Admin != 'Y')
    header("refresh: 0; URL=Admin.php");
    die();
 }
+
+$awardsGold   = 0;
+$awardsSilver = 0;
+$awardsBronze = 0;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en">
@@ -18,100 +22,8 @@ if ($Admin != 'Y')
     </head>
 
     <body bgcolor="White">
-    <h1 align="center">Summary Award Counts</h1>
-    <hr>
-    <?php
-         $results = mysql_query("select Award,
-                                        count(*) AwardCount
-                                 from   LTC_PHX_Registration
-                                 where  Award != 'No Award'
-                                 and    Award != 'No Show'
-                                 and    Award IS NOT NULL
-                                 group  by Award
-                                 order  BY Award")
-                    or die ("Unable to get summary award list:" . mysql_error());
-         ?>
-         <table border="1" width="100%" id="table1">
-            <tr>
-               <td bgcolor="#000000"><font color="#FFFF00">Gold</font></td>
-               <td bgcolor="#000000"><font color="#FFFF00">Silver</font></td>
-               <td bgcolor="#000000"><font color="#FFFF00">Bronze</font></td>
-            </tr>
-         <?php
-         while ($row = mysql_fetch_assoc($results))
-         {
-            $Award       = $row['Award'];
-            $AwardCount  = $row['AwardCount'];
 
-            if ($Award == 'Gold')
-               $GoldCount = $AwardCount;
-            else if ($Award == 'Bronze')
-               $BronzeCount = $AwardCount;
-            else if ($Award == 'Silver')
-            {
-               $SilverCount = $AwardCount;
-               ?>
-               <tr>
-                  <td><?php  print $GoldCount; ?></td>
-                  <td><?php  print $SilverCount; ?></td>
-                  <td><?php  print $BronzeCount; ?></td>
-               </tr>
-               <?php
-            }
-         }
-         ?>
-         </table>
-
-        <h1 align="center">Award Counts By Church</h1>
-    <hr>
-    <?php
-         $results = mysql_query("select ChurchName,
-                                        Award,
-                                        count(*) AwardCount
-                                 from   LTC_PHX_Registration r,
-                                        LTC_PHX_Churches c
-                                 where  Award != 'No Award'
-                                 and    Award != 'No Show'
-                                 and    Award IS NOT NULL
-                                 and    r.ChurchID = c.ChurchID
-                                 group  BY ChurchName,Award")
-                    or die ("Unable to get award list by church:" . mysql_error());
-         ?>
-         <table border="1" width="100%" id="table1">
-            <tr>
-               <td bgcolor="#000000"><font color="#FFFF00">Church Name</font></td>
-               <td bgcolor="#000000"><font color="#FFFF00">Gold</font></td>
-               <td bgcolor="#000000"><font color="#FFFF00">Silver</font></td>
-               <td bgcolor="#000000"><font color="#FFFF00">Bronze</font></td>
-                           </tr>
-         <?php
-         while ($row = mysql_fetch_assoc($results))
-         {
-            $ChurchName  = $row['ChurchName'];
-            $Award       = $row['Award'];
-            $AwardCount  = $row['AwardCount'];
-
-            if ($Award == 'Gold')
-               $GoldCount = $AwardCount;
-            else if ($Award == 'Bronze')
-               $BronzeCount = $AwardCount;
-            else if ($Award == 'Silver')
-            {
-               $SilverCount = $AwardCount;
-            ?>
-               <tr>
-                  <td><?php  print $ChurchName; ?></td>
-                  <td><?php  print $GoldCount; ?></td>
-                  <td><?php  print $SilverCount; ?></td>
-                  <td><?php  print $BronzeCount; ?></td>
-               </tr>
-               <?php
-            }
-         }
-         ?>
-         </table>
-
-        <h1 align="center">Award Counts By Event</h1>
+    <h1 align="center">Award Counts By Individual Event</h1>
     <hr>
     <?php
          $results = mysql_query("select   e.EventName,
@@ -119,8 +31,9 @@ if ($Admin != 'Y')
                                           count(*) as AwardCount
                                  from     $RegistrationTable r,
                                           $EventsTable e
-                                 where    r.EventID=e.EventID
-                                 and      r.Award != 'No Show'
+                                 where    r.EventID    = e.EventID
+                                 and      r.Award     != 'No Show'
+                                 and      e.TeamEvent  = 'N'
                                  group by e.EventName,
                                           r.Award")
                     or die ("Unable to get award list:" . mysql_error());
@@ -145,8 +58,11 @@ if ($Admin != 'Y')
                $BronzeCount = $AwardCount;
             else if ($Award == 'Silver')
             {
-               $SilverCount = $AwardCount;
-            ?>
+               $SilverCount   = $AwardCount;
+               $awardsGold   += $GoldCount;
+               $awardsSilver += $SilverCount;
+               $awardsBronze += $BronzeCount;
+               ?>
                <tr>
                   <td><?php  print $EventName; ?></td>
                   <td><?php  print $GoldCount; ?></td>
@@ -159,6 +75,74 @@ if ($Admin != 'Y')
          ?>
          </table>
 
-    </body>
+        <h1 align="center">Award Counts By Team Event</h1>
+    <hr>
+    <?php
+         $results = mysql_query("select e.EventName,
+                                        r.Award,
+                                        count(*) as AwardCount
+                                 from   LTC_PHX_Events       e,
+                                        LTC_PHX_Registration r,
+                                        LTC_PHX_Teams        t,
+                                        LTC_PHX_TeamMembers  m
+                                 where  e.EventID=t.EventID
+                                 and    t.TeamID=m.TeamID
+                                 and    m.ParticipantID=r.ParticipantID
+                                 group by e.EventName,r.Award;")
+                    or die ("Unable to get award list:" . mysql_error());
+         ?>
+         <table border="1" width="100%" id="table1">
+            <tr>
+               <td bgcolor="#000000"><font color="#FFFF00">Event Name</font></td>
+               <td bgcolor="#000000"><font color="#FFFF00">Gold</font></td>
+               <td bgcolor="#000000"><font color="#FFFF00">Silver</font></td>
+               <td bgcolor="#000000"><font color="#FFFF00">Bronze</font></td>
+            </tr>
+         <?php
+         while ($row = mysql_fetch_assoc($results))
+         {
+            $EventName   = $row['EventName'];
+            $Award       = $row['Award'];
+            $AwardCount  = $row['AwardCount'];
+
+            if ($Award == 'Gold')
+               $GoldCount = $AwardCount;
+            else if ($Award == 'Bronze')
+               $BronzeCount = $AwardCount;
+            else if ($Award == 'Silver')
+            {
+               $SilverCount = $AwardCount;
+               $awardsGold   += $GoldCount;
+               $awardsSilver += $SilverCount;
+               $awardsBronze += $BronzeCount;
+               ?>
+               <tr>
+                  <td><?php  print $EventName; ?></td>
+                  <td><?php  print $GoldCount; ?></td>
+                  <td><?php  print $SilverCount; ?></td>
+                  <td><?php  print $BronzeCount; ?></td>
+               </tr>
+               <?php
+            }
+         }
+         ?>
+         </table>
+    <hr>
+    <h1 align="center">Summary Award Counts</h1>
+    <hr>
+    <table border="1" width="100%" id="table1">
+       <tr>
+          <td bgcolor="#000000"><font color="#FFFF00">Gold</font></td>
+          <td bgcolor="#000000"><font color="#FFFF00">Silver</font></td>
+          <td bgcolor="#000000"><font color="#FFFF00">Bronze</font></td>
+       </tr>
+       <tr>
+          <td><?php  print $awardsGold; ?></td>
+          <td><?php  print $awardsSilver; ?></td>
+          <td><?php  print $awardsBronze; ?></td>
+      </tr>
+   </table>
+
+   </body>
 
 </html>
