99762a9d8b1451c501af0960b4904878e4cc2047
Paul Lemmons <paul@lrmmons.name>
2017-01-13 17:01:54 -0700

    Add a couple todo's in the reset functions

--- registration/include/table/resets.php
+++ registration/include/table/resets.php
@@ -1,30 +1,37 @@
 <?php
 
-function reset_new($Userid) {
-    global $ResetsTable;
-    $code    = reset_generate_code();
-    $created = time();
-    $sql = "INSERT INTO $ResetsTable
-               (Userid, Code, Created)
-               VALUES (".escape($Userid).",
-                       ".escape($code).",
-                       $created
-                      )
-            ";
-    Query($sql);
-    return $code;
+function reset_new($Userid)
+{
+   //TODO: Delete any old entries
+   //TODO: Delete any entries for this user if they exist
+
+   global $ResetsTable;
+   $code    = reset_generate_code();
+   $created = time();
+   $sql = "INSERT INTO $ResetsTable
+              (Userid, Code, Created)
+              VALUES (".escape($Userid).",
+                      ".escape($code).",
+                      $created
+                     )
+           ";
+   Query($sql);
+   return $code;
 }
 
-function reset_by_code($code){
-    global $ResetsTable;
-    return Fetch($ResetsTable, "Code = ".escape($code));
+function reset_by_code($code)
+{
+   global $ResetsTable;
+   return Fetch($ResetsTable, "Code = ".escape($code));
 }
 
-function reset_generate_code() {
-    return substr(bin2hex(openssl_random_pseudo_bytes(32)), 0, 64);
+function reset_generate_code()
+{
+   return substr(bin2hex(openssl_random_pseudo_bytes(32)), 0, 64);
 }
 
-function resets_delete_by_id($id) {
-    global $ResetsTable;
-    return Query("DELETE FROM $ResetsTable WHERE Code = ".escape($id));
+function resets_delete_by_id($id)
+{
+   global $ResetsTable;
+   return Query("DELETE FROM $ResetsTable WHERE Code = ".escape($id));
 }
