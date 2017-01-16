070dbbb4d48db74eb257cb02cb52030acdd4c771
Paul Lemmons <paul@lemmons.name>
2017-01-15 23:51:37 -0700

    Needed to restrict report to Admins

--- registration/RptEventParticipation.php
+++ registration/RptEventParticipation.php
@@ -9,6 +9,11 @@
 <?php
 include 'include/RegFunctions.php';
 
+if ($Admin != 'Y')
+{
+   header("refresh: 0; URL=Admin.php");
+   die();
+}
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
