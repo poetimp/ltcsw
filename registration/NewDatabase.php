fa5bb9fb9ff7b6ee6e926e75eb652957b5d2daf6
Paul Lemmons <paul@lemmons.name>
2018-09-24 11:43:45 -0700

    Move require of regfuncts ahead of HTML lines to preserver session data

--- registration/NewDatabase.php
+++ registration/NewDatabase.php
@@ -5,6 +5,7 @@
 //
 // Author: Paul Lemmons
 //----------------------------------------------------------------------------
+include 'include/RegFunctions.php';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en">
@@ -13,7 +14,6 @@
 //    print ("Can not clear database. It has already been done.");
 //    die();
 
-include 'include/RegFunctions.php';
 
 if ($Admin != 'Y')
 {
